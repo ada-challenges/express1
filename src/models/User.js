3737a79866a7941dbcda5f8e88f1b685b440e3ab
AlleCardos <dev.allexandrecp@gmail.com>
2024-01-28 22:12:12 -0300

    Criado Hash e Salt

--- src/models/User.js
+++ src/models/User.js
@@ -1,42 +1,39 @@
 const Model = require("./model");
+const crypto = require('crypto');
 
-class User extends Model{
+class User extends Model {
 
-    constructor(){
+    constructor() {
         super()
         this.table = 'users'
     }
 
-    async getAll(fields,whereClause) {
-        const result = await super.select(fields,whereClause);
+    async select(fields, whereClause) {
+        const result = await super.select(fields, whereClause);
         return result;
     }
 
-    async createUser(data) {
+    async insert(data) {
+        const { password } = data
+
+        const salt = crypto.randomBytes(16).toString('hex'); //salt
+        const hash = crypto.createHash('sha256').update(password + salt).digest('hex'); //permanente
+
+        data.password = hash;
+        data.salt = salt;
+
         const result = await super.insert(data);
-        if(result.rowCount > 0){
-            console.log(result.rows);
-        }else{
-            console.log('Não foi possivél cadastrar o usuário');
-        }
-    }
-    
-    async deleteUser(whereClause) {
-        const result = await super.delete(whereClause);
-        if(result.rowCount === 0){
-            console.log(result.rows);
-        }else{
-            console.log('Não foi possivél deletar o usuário');
-        }
+        return result;
     }
 
-    async updateUser(data,whereClause) {
-        const result = await super.update(data,whereClause);
-        // if(result.rowCount > 0){
-        //     console.log(result.rows);
-        // }else{
-        //     console.log('Não foi possivél atualizar o usuário');
-        // }
+    async delete(whereClause) {
+        const result = await super.delete(whereClause);
+        return result;
+    }
+
+    async update(data, whereClause) {
+        const result = await super.update(data, whereClause);
+        return result;
     }
 
 }
