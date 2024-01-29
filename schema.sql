b2ba7428c664e8293a19c3d5c3a9770a673d266b
AlleCardos <dev.allexandrecp@gmail.com>
2024-01-28 22:08:06 -0300

    Remoção Dados Json (Banco Criado)

--- schema.sql
+++ schema.sql
@@ -5,7 +5,9 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(60) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    role VARCHAR(60) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -21,23 +23,3 @@ CREATE TABLE products (
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
--- Inserir registros fictícios na tabela "users"
-INSERT INTO users (name, email, password) VALUES
-    ('Eva', 'eva@example.com', 'hashed_password_4'),
-    ('David', 'david@example.com', 'hashed_password_5'),
-    ('Fiona', 'fiona@example.com', 'hashed_password_6'),
-    ('Alice', 'alice@example.com', 'hashed_password_1'),
-    ('Bob', 'bob@example.com', 'hashed_password_2'),
-    ('Charlie', 'charlie@example.com', 'hashed_password_3');
-
--- Inserir registros fictícios na tabela "products"
-INSERT INTO products (name, description, price, category, photo_path, user_id) VALUES
-    ('Product 1', 'Description for Product 1', 19.99, 'Category 1', '/photos/product1.jpg', 1),
-    ('Product 2', 'Description for Product 2', 29.99, 'Category 2', '/photos/product2.jpg', 2),
-    ('Product 3', 'Description for Product 3', 39.99, 'Category 1', '/photos/product3.jpg', 3),
-    ('Product 4', 'Description for Product 4', 49.99, 'Category 3', '/photos/product4.jpg', 1),
-    ('Product 5', 'Description for Product 5', 59.99, 'Category 2', '/photos/product5.jpg', 2),
-    ('Product 6', 'Description for Product 6', 69.99, 'Category 1', '/photos/product6.jpg', 3),
-    ('Product 7', 'Description for Product 7', 79.99, 'Category 3', '/photos/product7.jpg', 1),
-    ('Product 8', 'Description for Product 8', 89.99, 'Category 2', '/photos/product8.jpg', 2);
